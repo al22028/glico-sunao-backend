8ddc69093d831b111bf07776d650606e92808f84
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-03-20 04:51:26 +0900

    add bgl create request schema

--- src/v1/schemas/bgl.py
+++ src/v1/schemas/bgl.py
@@ -1,5 +1,9 @@
+# Standard Library
+from datetime import datetime
+
 # Third Party Library
 from schemas.base import BaseSchema
+from schemas.event_timing import EventTiming
 
 
 class BGLSchema(BaseSchema):
@@ -8,5 +12,13 @@ class BGLSchema(BaseSchema):
     value: float
     event_timing: str
     record_time: str
+    is_deleted: bool
     created_at: str
     updated_at: str
+
+
+class BGLCreateRequestSchema(BaseSchema):
+    user_id: str
+    value: float
+    event_timing: EventTiming
+    record_time: datetime
