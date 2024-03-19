8ccbe808ac462b46e8312b6d647808c7779d324f
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-03-20 04:51:13 +0900

    add model orm find and create one

--- src/v1/models/bgl.py
+++ src/v1/models/bgl.py
@@ -2,7 +2,9 @@
 from typing import List
 
 # Third Party Library
+from aws_lambda_powertools.event_handler.exceptions import NotFoundError
 from database.base import BGLModel
+from schemas.bgl import BGLCreateRequestSchema
 
 
 class BGLModelORM:
@@ -10,3 +12,15 @@ class BGLModelORM:
     def find_all(self) -> List[BGLModel]:
         items = BGLModel.scan()
         return [item for item in items]
+
+    def create_one(self, data: BGLCreateRequestSchema) -> BGLModel:
+        item = BGLModel(**data.model_dump())
+        item.save()
+        return item
+
+    def find_one(self, id: str) -> BGLModel:
+        item = BGLModel.scan(BGLModel.id == id, limit=1)
+        try:
+            return item.next()
+        except StopIteration:
+            raise NotFoundError(f"BGL data not found with id: {id}")
