e3d1506dcde1f0920a3d66f2de3484a78c770aee
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-06-19 21:08:02 +0900

    ✨ Add user delete endpoint #120

--- src/v1/services/user_service.py
+++ src/v1/services/user_service.py
@@ -2,6 +2,7 @@
 from typing import List
 
 # Third Party Library
+from aws_lambda_powertools.event_handler.exceptions import NotFoundError
 from repositories.user_repository import UserRepository
 from schemas.user import UserCreateRequestSchema, UserSchema
 
@@ -16,6 +17,8 @@ class UserService:
         return [item.serializer() for item in items]
 
     def find_one(self, user_id: str) -> UserSchema:
+        if not self.repository.is_exist(user_id):
+            raise NotFoundError("User not found")
         data = self.repository.find_one(user_id)
         return data.serializer()
 
@@ -24,5 +27,13 @@ class UserService:
         return item.serializer()
 
     def update_term_agreed_at(self, user_id: str) -> UserSchema:
+        if not self.repository.is_exist(user_id):
+            raise NotFoundError("User not found")
         item = self.repository.update_term_agreed_at(user_id)
+        return item.serializer()
+
+    def delete_one(self, user_id: str) -> UserSchema:
+        if not self.repository.is_exist(user_id):
+            raise NotFoundError("User not found")
+        item = self.repository.delete_one(user_id)
         return item.serializer()
