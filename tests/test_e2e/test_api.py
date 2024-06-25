921a0ee73a60fd959fd8440974607d6c0fbe57db
zaki-t <zaki.t@world-wing.com>
2024-06-25 10:55:52 +0900

    bgl test complete

--- tests/test_e2e/test_api.py
+++ tests/test_e2e/test_api.py
@@ -2,15 +2,15 @@
 import json
 
 # Third Party Library
-import requests
+import requests  # type: ignore
 
-BASE_URL = "https://api.d02.teba-saki.net"
+BASE_URL = "https://api.d02.teba-saki.net/v1"
 EXAMPLE_DATA = {
-  "value": 89,
-  "eventTiming": "食後",
-  "recordTime": "2024-06-18T17:46:10.020916",
-  "sunaoFood": "パスタ",
-  "userId": "6d4c5a63b4b64b1283a4bc4b8f97939d"
+    "value": 89,
+    "eventTiming": "食後",
+    "recordTime": "2024-06-18T17:46:10.020916",
+    "sunaoFood": "パスタ",
+    "userId": "6d4c5a63b4b64b1283a4bc4b8f97939d",
 }
 
 
@@ -18,54 +18,77 @@ class BGL:
     def __init__(self) -> None:
         self.endpoint = f"{BASE_URL}/bgl"
         self.example_value = EXAMPLE_DATA
-        self.id = None
+        self.headers = {"Content-Type": "application/json"}
+        self.id: str | None = None
 
-    def get_all_test(self) -> None:
+    def test_post(self) -> None:
+        json_data = json.dumps(self.example_value)
+        res = requests.post(self.endpoint, data=json_data, headers=self.headers)
+        assert res.status_code == 200, print(res.status_code)
+        print("post test passed!")
+        self.id = res.json()["id"]
+
+    def test_get_all(self) -> None:
         res = requests.get(self.endpoint)
-        print(res)
+        assert res.status_code == 200, print(res.status_code)
+        print("get all test passed!")
 
-    def get_test(self) -> None:
-        if self.id != None:
+    def test_get(self) -> None:
+        if self.id is not None:
             _url = f"{self.endpoint}/{self.id}"
             res = requests.get(_url)
-            if res.status_code == 200:
-                print(f"get request: {res}")
-            else:
-                print(res.json())
+            assert res.status_code == 200, print(res.status_code)
+            print("get test passed!")
         else:
             print("ID is not found.")
 
-    def post_test(self) -> None:
-        headers = {"Content-Type": "application/json"}
-        json_data = json.dumps(self.example_value)
-        res = requests.post(self.endpoint, data=json_data, headers=headers)
-        if res.status_code == 201:
-            self.id = res.json()["id"]
-            print(self.id)
+    def test_get_by_query(self) -> None:
+        if self.id is not None:
+            _url = f"{self.endpoint}/{self.id}"
+            _params = {"userID": self.id, "from": "20240618", "to": "20240718"}
+            res = requests.get(_url, params=_params)
+            assert res.status_code == 200, print(res.status_code)
+            print("get by query test passed!")
+        else:
+            print("ID is not found.")
 
-    def delete_test(self) -> None:
-        if self.id != None:
+    def test_put(self) -> None:
+        if self.id is not None:
+            _url = f"{self.endpoint}/{self.id}"
+            _updated_data = self.example_value.copy()
+            _updated_data["sunaoFood"] = "うどん"
+            json_data = json.dumps(_updated_data)
+            res = requests.put(_url, data=json_data, headers=self.headers)
+            assert res.status_code == 200, print(res.status_code)
+            print("put test passed!")
+        else:
+            print("ID is not found.")
+
+    def test_delete(self) -> None:
+        if self.id is not None:
             _url = f"{self.endpoint}/{self.id}"
             res = requests.delete(_url)
-            if res.status_code == 200:
-                print(f"delete request: {res}")
-            else:
-                print(res.json())
+            assert res.status_code == 200, print(res.status_code)
+            print("delete test passed!")
         else:
             print("ID is not found.")
 
     def test(self) -> None:
+        print("[bgl test]")
         try:
-            self.post_test()
-            self.get_test()
-            self.delete_test()
-        except RuntimeError as e:
-            raise(e)
+            self.test_post()
+            self.test_get_all()
+            self.test_get()
+            self.test_get_by_query()
+            self.test_put()
+            self.test_delete()
+        except AssertionError as a:
+            raise (a)
+
 
 def main() -> None:
     bgl = BGL()
     bgl.test()
-
 
 
 if __name__ == "__main__":
